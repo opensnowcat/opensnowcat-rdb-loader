0761827118381dad9b603e85122753297a916f3c
Scala Steward <scala-steward@users.noreply.github.com>
2024-01-21 01:23:57 +0000

    Bump schema-ddl from 0.18.2 to 0.21.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -47,7 +47,7 @@ object Dependencies {
 
     val spark = "3.3.1"
     val eventsManifest = "0.3.0"
-    val schemaDdl = "0.18.2"
+    val schemaDdl = "0.21.0"
     val jacksonModule = "2.14.2" // Override incompatible version in spark runtime
     val jacksonDatabind = "2.14.3"
     val parquet4s = "2.10.0"
