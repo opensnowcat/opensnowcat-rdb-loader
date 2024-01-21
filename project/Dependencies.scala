6a788df5ca590cb188ff1b7cb6b968a623db1d5c
Scala Steward <scala-steward@users.noreply.github.com>
2024-01-21 01:25:40 +0000

    Bump sentry from 1.7.30 to 7.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -67,7 +67,7 @@ object Dependencies {
     val aws = "1.12.261"
     val aws2 = "2.18.8"
     val jSch = "0.2.1"
-    val sentry = "1.7.30"
+    val sentry = "7.2.0"
     val protobuf = "3.21.7" // Fix CVE
     val kinesisClient = "2.4.3"
     val nettyCodec = "4.1.86.Final" // Fix CVE
