871832d7c19ef547c9191a84e12a4dc630ad75c9
Scala Steward <scala-steward@users.noreply.github.com>
2024-01-21 01:28:05 +0000

    Bump log4cats-slf4j from 2.5.0 to 2.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -40,7 +40,7 @@ object Dependencies {
     val doobie = "1.0.0-RC2"
     val monocle = "2.0.3"
     val catsRetry = "3.1.0"
-    val log4cats = "2.5.0"
+    val log4cats = "2.6.0"
     val http4s = "0.23.17"
     val http4sBlaze = "0.23.14" // this dep fell out of sync with http4s-core versioning - 0.23.14 is the last 0.X release.
     val scalaTracker = "2.0.0"
