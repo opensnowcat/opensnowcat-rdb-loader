ede1b4e9ecde604f1712a94511e033326561c839
Scala Steward <scala-steward@users.noreply.github.com>
2024-02-25 01:14:10 +0000

    Bump aws-java-sdk-cloudwatch, ... from 1.12.662 to 1.12.666

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -64,7 +64,7 @@ object Dependencies {
     val redshiftJdbc = "1.2.55.1083"
     val snowflakeJdbc = "3.15.0"
     val enumeratum = "1.7.3"
-    val aws = "1.12.662"
+    val aws = "1.12.666"
     val aws2 = "2.24.6"
     val jSch = "0.2.16"
     val sentry = "1.7.30"
