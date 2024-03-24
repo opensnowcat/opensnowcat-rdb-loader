bbd118a419a8d65af393e6c43b1ed7cf99b80edf
Scala Steward <scala-steward@users.noreply.github.com>
2024-03-24 01:18:04 +0000

    Bump google-cloud-storage from 2.36.0 to 2.36.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -34,7 +34,7 @@ object Dependencies {
     val fs2PubSub = "0.21.0"
     val fs2Kafka = "3.3.1"
     val secretManager = "2.37.0"
-    val gcpStorage = "2.36.0"
+    val gcpStorage = "2.36.1"
     val azureIdentity = "1.9.0"
     val azureKeyVault = "4.6.2"
     val doobie = "1.0.0-RC5"
