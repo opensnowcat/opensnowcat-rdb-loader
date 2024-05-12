05e1accbfcf8260d27a3960ef07d83fbc5962676
Scala Steward <scala-steward@users.noreply.github.com>
2024-05-12 01:22:21 +0000

    Bump nimbus-jose-jwt from 9.31 to 9.39

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -55,7 +55,7 @@ object Dependencies {
     val hadoopGcpClient = "hadoop3-2.2.5"
     val parquetHadoop = "1.13.1"
     val jsonSmart = "2.5.1" // Fix CVE
-    val nimbusJose = "9.31" // Fix CVE
+    val nimbusJose = "9.39" // Fix CVE
     val snappyJava = "1.1.10.5" // Fix CVE
     val commonsText = "1.10.0" // Fix CVE
     val ivy = "2.5.2" // Fix CVE
