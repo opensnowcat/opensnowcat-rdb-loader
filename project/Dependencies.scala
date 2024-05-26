3994c2782e79ae530cbee828d31e99ab3a1e96d6
Scala Steward <scala-steward@users.noreply.github.com>
2024-05-26 01:22:48 +0000

    Bump kinesis, regions, s3, sns, sqs, sts from 2.25.55 to 2.25.60

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -65,7 +65,7 @@ object Dependencies {
     val snowflakeJdbc = "3.16.0"
     val enumeratum = "1.7.3"
     val aws = "1.12.725"
-    val aws2 = "2.25.55"
+    val aws2 = "2.25.60"
     val jSch = "0.2.17"
     val sentry = "1.7.30"
     val protobuf = "4.26.1" // Fix CVE
