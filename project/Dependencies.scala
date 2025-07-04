dbcdaaf1dc1760e4e3b9309aa72d9c35f318d627
Oleh Yermolaiev <oleg.yermolaiev@sysgears.com>
2025-07-04 17:08:44 +0300

    Update AWS SDKs

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -64,8 +64,8 @@ object Dependencies {
     val redshiftJdbc = "1.2.55.1083"
     val snowflakeJdbc = "3.16.0"
     val enumeratum = "1.7.4"
-    val aws = "1.12.761"
-    val aws2 = "2.26.21"
+    val aws = "1.12.787"
+    val aws2 = "2.31.76"
     val jSch = "0.2.17"
     val sentry = "1.7.30"
     val protobuf = "4.27.2" // Fix CVE
